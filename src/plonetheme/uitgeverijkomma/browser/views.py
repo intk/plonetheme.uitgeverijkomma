98a35d3afeb03716b6d81d6323e3a075980343cd
andreesg <andreslb1@gmail.com>
2021-07-06 15:56:15 +0200

    Product specs behavior

--- src/plonetheme/uitgeverijkomma/browser/views.py
+++ src/plonetheme/uitgeverijkomma/browser/views.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from Products.Five import BrowserView
+from plonetheme.uitgeverijkomma import _
+
+from zope.interface import provider
+from plone.autoform import directives as form
+from plone.autoform.interfaces import IFormFieldProvider
+from plone.app.textfield import RichText as RichTextField
+from plone.app.z3cform.widget import RichTextFieldWidget
+from plone.supermodel import model
+from zope import schema
+from collective import dexteritytextindexer
 
 class ContextToolsView(BrowserView):
     def trimText(self, text, limit):
@@ -17,4 +27,25 @@ class ContextToolsView(BrowserView):
                 return ""
         except:
             return text
+
+
+
+@provider(IFormFieldProvider)
+class IBook(model.Schema):
+    """Interface for Books behavior."""
+
+    # exhibition fieldset
+    model.fieldset(
+        'book',
+        label=_(u'Book', default=u'Book'),
+        fields=['specs'],
+    )
+
+    specs = RichTextField(
+        title=_(u'Specs'),
+        description=u'',
+        required=False,
+    )
+    form.widget('specs', RichTextFieldWidget)
+    dexteritytextindexer.searchable('notes')
 
