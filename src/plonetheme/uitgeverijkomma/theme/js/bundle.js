2489c253fb48ab2a5b925d21aef0dc5cc1f8d9de
andreesg <andreslb1@gmail.com>
2021-07-08 17:12:14 +0200

    Menu top bar

--- src/plonetheme/uitgeverijkomma/theme/js/bundle.js
+++ src/plonetheme/uitgeverijkomma/theme/js/bundle.js
@@ -33,22 +33,81 @@ function getCookie(name) {
 })();
 
 function load_menu() {
-	var isLateralNavAnimating = false;
-	var menu_buttons = document.getElementsByClassName("cd-nav-trigger-wrapper");
+	var mainHeader = $('#mainnavigation-wrapper'),
+		secondaryNavigation = $('.cd-secondary-nav'),
+		//this applies only if secondary nav is below intro section
+		belowNavHeroContent = $('.sub-nav-hero'),
+		headerHeight = mainHeader.height();
+	
+	//set scrolling variables
+	var scrolling = false,
+		previousTop = 0,
+		currentTop = 0,
+		scrollDelta = 10,
+		scrollOffset = 150;
 
-	var trigger_menu_button = function(event) {		
-		if( !isLateralNavAnimating ) {
-	      	if (jQuery(this).parents('.csstransitions').length > 0) isLateralNavAnimating = true; 
-      		jQuery('body').toggleClass('navigation-is-open');
-      
-      		jQuery('.cd-navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function() {
-	        	isLateralNavAnimating = false;
-	      	});
+	$(window).on('scroll', function(){
+		if( !scrolling ) {
+			scrolling = true;
+			(!window.requestAnimationFrame)
+				? setTimeout(autoHideHeader, 250)
+				: requestAnimationFrame(autoHideHeader);
+		}
+	});
+
+	$(window).on('resize', function(){
+		headerHeight = mainHeader.height();
+	});
+
+	function autoHideHeader() {
+		var currentTop = $(window).scrollTop();
+
+		( belowNavHeroContent.length > 0 ) 
+			? checkStickyNavigation(currentTop) // secondary navigation below intro
+			: checkSimpleNavigation(currentTop);
+
+	   	previousTop = currentTop;
+		scrolling = false;
+	}
+
+	function checkSimpleNavigation(currentTop) {
+		//there's no secondary nav or secondary nav is below primary nav
+	    if (previousTop - currentTop > scrollDelta) {
+	    	//if scrolling up...
+	    	mainHeader.removeClass('is-hidden');
+	    } else if( currentTop - previousTop > scrollDelta && currentTop > scrollOffset) {
+	    	//if scrolling down...
+	    	mainHeader.addClass('is-hidden');
 	    }
-	};
+	}
 
-	for (var i = 0; i < menu_buttons.length; i++) {
-	    menu_buttons[i].addEventListener('click', trigger_menu_button, {passive: true});
+	function checkStickyNavigation(currentTop) {
+		//secondary nav below intro section - sticky secondary nav
+		var secondaryNavOffsetTop = belowNavHeroContent.offset().top - secondaryNavigation.height() - mainHeader.height();
+		
+		if (previousTop >= currentTop ) {
+	    	//if scrolling up... 
+	    	if( currentTop < secondaryNavOffsetTop ) {
+	    		//secondary nav is not fixed
+	    		mainHeader.removeClass('is-hidden');
+	    		secondaryNavigation.removeClass('fixed slide-up');
+	    		belowNavHeroContent.removeClass('secondary-nav-fixed');
+	    	} else if( previousTop - currentTop > scrollDelta ) {
+	    		//secondary nav is fixed
+	    		mainHeader.removeClass('is-hidden');
+	    	}
+	    	
+	    } else {
+	    	//if scrolling down...	
+	 	  	if( currentTop > secondaryNavOffsetTop + scrollOffset ) {
+	 	  		//hide primary nav
+	    		mainHeader.addClass('is-hidden');
+	    	} else if( currentTop > secondaryNavOffsetTop ) {
+	    		//once the secondary nav is fixed, do not hide primary nav if you haven't scrolled more than scrollOffset 
+	    		mainHeader.removeClass('is-hidden');
+	    	}
+
+	    }
 	}
 };
 
